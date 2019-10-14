533eec730233869efa1261827402f5de7c5e4276
Angelique Vannespenne <angeliquevannespenne@hotmail.fr>
2019-10-14 08:28:14 +0200

    First Commit

---
+++ src/HelloUniverse.java
@@ -0,0 +1,13 @@
+public class HelloUniverse {
+
+    public static void main(String... args){
+        System.out.println("Aux dernieres nouvelles, le nombre total de planèetes dans le systeme solaire est de : ");
+        int nbPlanet=8;
+        System.out.println(nbPlanet);
+
+        System.out.print("Il y a quelques années cependant, elles etaient au nombre de :");
+        nbPlanet=9;
+        System.out.println(nbPlanet);
+    }
+
+}
