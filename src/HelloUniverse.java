6885ed4242d85a1a3014f8a8ea308e0c22032be9
Angelique Vannespenne <angeliquevannespenne@hotmail.fr>
2019-10-14 09:19:54 +0200

    int calcul variable

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,14 +1,10 @@
+import javax.xml.soap.SOAPPart;
+
 public class HelloUniverse {
 
     public static void main(String... args){
-        String debutPhrase="Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de : ";
-        System.out.println(debutPhrase);
-        int nbPlanetes=9;
-        System.out.println(nbPlanetes);
-        String finPhrase="Il y a quelques années cependant, elles étaient au nombre de : ";
-        System.out.println(finPhrase);
-        nbPlanetes=8;
-        System.out.println(nbPlanetes);
+        int somme=5+2;
+        System.out.println(somme);
     }
 
 }
