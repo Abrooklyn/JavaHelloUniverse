56b6fc9cc26295d0c27b2ea95a19450eddbccc91
Angelique Vannespenne <angeliquevannespenne@hotmail.fr>
2019-10-14 08:39:29 +0200

    second exo

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,13 +1,14 @@
 public class HelloUniverse {
 
     public static void main(String... args){
-        System.out.println("Aux dernieres nouvelles, le nombre total de planèetes dans le systeme solaire est de : ");
-        int nbPlanet=8;
-        System.out.println(nbPlanet);
-
-        System.out.print("Il y a quelques années cependant, elles etaient au nombre de :");
-        nbPlanet=9;
-        System.out.println(nbPlanet);
+        String debutPhrase="Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de : ";
+        System.out.println(debutPhrase);
+        int nbPlanetes=9;
+        System.out.println(nbPlanetes);
+        String finPhrase="Il y a quelques années cependant, elles étaient au nombre de : ";
+        System.out.println(finPhrase);
+        nbPlanetes=8;
+        System.out.println(nbPlanetes);
     }
 
 }
